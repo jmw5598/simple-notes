47e7bd3a9ee966de97132788110925f47e2833b4
Jason White <jmw5598@gmail.com>
2018-04-09 10:41:50 -0400

    renamed file structure for test package

--- book-notes/src/test/java/xyz/jasonwhite/notes/NotesApplicationTests.java
+++ book-notes/src/test/java/xyz/jasonwhite/notes/NotesApplicationTests.java
@@ -1,4 +1,4 @@
-package xyz.jasonwhite.booknotes;
+package xyz.jasonwhite.notes;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BookNotesApplicationTests {
+public class NotesApplicationTests {
 
 	@Test
 	public void contextLoads() {
