a225419c7d3f2a19de14480cf837550da4f6658e
Jason White <jmw5598@gmail.com>
2018-04-09 10:46:17 -0400

    created TopicNotFoundException

---
+++ book-notes/src/main/java/xyz/jasonwhite/notes/repositories/exceptions/TopicNotFoundException.java
@@ -0,0 +1,16 @@
+package xyz.jasonwhite.notes.repositories.exceptions;
+
+public class TopicNotFoundException extends RuntimeException {
+        
+    private Long id;
+    
+    public TopicNotFoundException(final Long id) {
+        super("Book could not be found with id: " + id);
+        this.id = id;
+    }
+    
+    public Long getId() {
+        return this.id;
+    }
+    
+}
