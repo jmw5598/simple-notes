e361cea36be9e92287e19698b0511d82221cd9c5
Jason White <jmw5598@gmail.com>
2018-04-10 16:45:13 -0400

    update app.module

--- interface/src/app/app.module.ts
+++ interface/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app.routing';
+import { NavigationModule } from './navigation/navigation.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    NavigationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
