d445edf71c64eabdd010d7bbfd6eb28d21e4d723
Jason White <jmw5598@gmail.com>
2018-04-10 16:48:59 -0400

    created skeleton component for topics dashboard

---
+++ interface/src/app/topics/topic-dashboard/topic-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sn-topic-dashboard',
+  templateUrl: './topic-dashboard.component.html',
+  styleUrls: ['./topic-dashboard.component.css']
+})
+export class TopicDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
