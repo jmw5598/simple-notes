863363c6619174f42e4dd59c66b57780f62cfd34
Jason White <jmw5598@gmail.com>
2018-04-10 16:49:57 -0400

    updated topics module for routing

--- interface/src/app/topics/topics.module.ts
+++ interface/src/app/topics/topics.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TopicsRoutingModule } from './topics.routing';
+
+import { TopicDashboardComponent } from './topic-dashboard/topic-dashboard.component';
+import { TopicCreateComponent } from './topic-create/topic-create.component';
 
 @NgModule({
-  imports: [],
-  exports: [],
-  declarations: [],
+  imports: [
+    CommonModule,
+    TopicsRoutingModule
+  ],
+  exports: [TopicCreateComponent, TopicDashboardComponent],
+  declarations: [TopicCreateComponent, TopicDashboardComponent],
   providers: []
 })
 export class TopicsModule {}
