69e47d8b711726b2931d29dc705a027ebb28671c
Jason White <jmw5598@gmail.com>
2018-04-10 09:05:49 -0400

    extended abstract controller advice

--- simple-notes/src/main/java/xyz/jasonwhite/notes/controllers/advice/TopicControllerAdvice.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/controllers/advice/TopicControllerAdvice.java
@@ -1,7 +1,5 @@
 package xyz.jasonwhite.notes.controllers.advice;
 
-import java.util.Optional;
-
 import org.springframework.hateoas.VndErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +12,11 @@ import xyz.jasonwhite.notes.repositories.exceptions.TopicNotFoundException;
 
 @ControllerAdvice(assignableTypes = TopicController.class)
 @RequestMapping(produces = "application/vnd.error+json")
-public class TopicControllerAdvice {
+public class TopicControllerAdvice extends AbstractControllerAdvice {
     
     @ExceptionHandler(TopicNotFoundException.class)
     public ResponseEntity<VndErrors> handleBookNotFound(TopicNotFoundException e) {
         return error(e, HttpStatus.NOT_FOUND, String.valueOf(e.getId()));
-    }
-    
-    private <E extends Exception> ResponseEntity<VndErrors> error(
-        final E exception, final HttpStatus httpStatus, final String logRef) {
-        final String message = Optional.of(exception.getMessage()).orElse(exception.getClass().getSimpleName());
-        return new ResponseEntity<>(new VndErrors(logRef, message), httpStatus);
     }
 
 }
