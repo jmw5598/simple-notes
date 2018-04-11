7946ee2744bab412b2aed20affb3fd5d52387ddc
Jason White <jmw5598@gmail.com>
2018-04-11 15:57:32 -0400

    created topic-create (form) to create a new topic

--- interface/src/app/topics/topic-create/topic-create.component.ts
+++ interface/src/app/topics/topic-create/topic-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Topic } from '../../shared/model/topic.model';
+import { TopicsService } from '../../core/services/topics.service';
 
 @Component({
   selector: 'sn-topic-create',
@@ -7,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicCreateComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private topicsService: TopicsService
+  ) { }
 
   ngOnInit() {
-    console.log("new topic form");
+    this.form = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      synopsis: ['', [Validators.required]]
+    });
+  }
+
+  submit(topic: Topic) {
+    this.topicsService.create(topic)
+      .subscribe(
+        data => this.router.navigate(['../', data.id], { relativeTo: this.route }),
+        error => console.log("error saving new topic " + error)
+      );
   }
 
 }
