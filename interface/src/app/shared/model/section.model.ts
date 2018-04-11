ab5246060972b39fdb916c9641221ffaf7c1ac9b
Jason White <jmw5598@gmail.com>
2018-04-11 15:55:32 -0400

    updated topic and sections models

--- interface/src/app/shared/model/section.model.ts
+++ interface/src/app/shared/model/section.model.ts
@@ -1,4 +1,12 @@
 export class Section {
+
+  public id: number;
+  public title: string;
+  public synopsis: string;
+  public notes: string;
+  public createdOn: Date;
+  public lastModified: Date;
+
   constructor(
     id: number,
     title: string,
@@ -6,5 +14,12 @@ export class Section {
     notes: string,
     createdOn: Date,
     lastModified: Date
-  ) {}
+  ) {
+    this.id = id;
+    this.title = title;
+    this.synopsis = synopsis;
+    this.notes = notes;
+    this.createdOn = createdOn;
+    this.lastModified = lastModified;
+  }
 }
--- interface/src/app/shared/model/topic.model.ts
+++ interface/src/app/shared/model/topic.model.ts
@@ -1,12 +1,28 @@
 import { Section } from './section.model';
 
 export class Topic {
+
+  public id: number;
+  public title: string;
+  public synopsis: string;
+  public createdOn: Date;
+  public lastModified: Date;
+  public sections: Section[];
+
   constructor(
     id: number,
     title: string,
     synopsis: string,
     createdOn: Date,
     lastModified: Date,
-    sections: Section[]
-  ) {}
+    sections?: Section[]
+  ) {
+    this.id = id;
+    this.title = title;
+    this.synopsis = synopsis;
+    this.createdOn = createdOn;
+    this.lastModified = lastModified;
+    this.sections = this.sections;
+  }
+
 }
