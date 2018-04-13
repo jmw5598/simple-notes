d88e4a3d5b5ea13de4e8f3677c072736ccbb9e9a
Jason White <jmw5598@gmail.com>
2018-04-13 13:24:32 -0400

    updated topic details component, pulling in data and displaying sections in table.

--- interface/src/app/topics/topic-details/topic-details.component.ts
+++ interface/src/app/topics/topic-details/topic-details.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Topic } from '../../shared/model/topic.model';
+import { TopicsService } from '../../core/services/topics.service';
 
 @Component({
-  selector: 'app-topic-details',
+  selector: 'sn-topic-details',
   templateUrl: './topic-details.component.html',
   styleUrls: ['./topic-details.component.css']
 })
-export class TopicDetailsComponent implements OnInit {
+export class TopicDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private params: Subscription;
+  private id: number;
+  public topic: Topic;
+
+  constructor(
+    private route: ActivatedRoute,
+    private topicsService: TopicsService
+  ) { }
 
   ngOnInit() {
+    this.params = this.route.params
+      .subscribe(
+        params => {
+          this.id = +params['id']; // (+) converts string 'id' to a number
+          this.retrieveData();
+        }
+      );
+  }
+
+  delete(sectionId: number) {
+    console.log("deleting id...." + sectionId);
+  }
+
+  retrieveData() {
+    this.topicsService.findById(this.id)
+      .subscribe(
+        data => this.topic = data,
+        error => console.log("error getting data for id: " + this.id)
+      );
+  }
+
+  ngOnDestroy() {
+    if(this.params)
+      this.params.unsubscribe();
   }
 
 }
