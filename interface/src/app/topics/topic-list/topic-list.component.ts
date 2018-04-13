c9396afbe59d84a2ae9f04457183d36854a67692
Jason White <jmw5598@gmail.com>
2018-04-13 13:25:48 -0400

    updated topic list component to display inputted list. emits delete event when delete button is clicked

--- interface/src/app/topics/topic-list/topic-list.component.ts
+++ interface/src/app/topics/topic-list/topic-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Topic } from '../../shared/model/topic.model';
 
@@ -12,9 +12,16 @@ export class TopicListComponent implements OnInit {
   @Input()
   topics: Topic[];
 
+  @Output()
+  onDelete: EventEmitter<number> = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  delete(id: number) {
+    this.onDelete.emit(id);
   }
 
 }
