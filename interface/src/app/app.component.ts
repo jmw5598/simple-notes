d593c799ac216d1a06ad78489c07aa9682ec7ebb
Jason White <jmw5598@gmail.com>
2018-04-13 13:04:19 -0400

    added faux login logic

--- interface/src/app/app.component.ts
+++ interface/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { AuthenticationService } from './core/services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  login = true;
+
+  public isAuthenticated: boolean;
+  private authenticated: Subscription;
+
+  constructor(private authenticationService: AuthenticationService) {}
+
+  ngOnInit() {
+    this.authenticationService.authentication
+      .subscribe(
+        data => this.isAuthenticated = data,
+        error => console.log("error subscribing to authencation")
+      );
+  }
+
+  ngOnDestroy() {
+    if(this.authenticated)
+      this.authenticated.unsubscribe();
+  }
+
 }
