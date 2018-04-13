7eb4128998e65972c20cf53d49ee6da847dc7f6b
Jason White <jmw5598@gmail.com>
2018-04-13 13:05:48 -0400

    added core module import

--- interface/src/app/app.module.ts
+++ interface/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app.routing';
+import { CoreModule } from './core/core.module';
 import { NavigationModule } from './navigation/navigation.module';
 
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    CoreModule,
     NavigationModule,
   ],
   providers: [],
