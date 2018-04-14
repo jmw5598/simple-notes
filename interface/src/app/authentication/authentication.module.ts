1d3aaf3cd8249f1bbd4ac0db41b18b5b51d873fe
Jason White <jmw5598@gmail.com>
2018-04-14 09:48:22 -0400

    created authentication module and moved authencation service to module

---
+++ interface/src/app/authentication/authentication.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { AuthenticationService } from './services/authentication.service';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [AuthenticationService],
+  providers: [AuthenticationService]
+})
+export class AuthenticationModule {}
