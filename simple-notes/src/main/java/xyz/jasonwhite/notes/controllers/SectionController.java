f4c29a5b39038e2f684309a95b697bdd2ef7adec
Jason White <jmw5598@gmail.com>
2018-04-14 13:38:10 -0400

    initial commit for sections controller

---
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/controllers/SectionController.java
@@ -0,0 +1,10 @@
+package xyz.jasonwhite.notes.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/topics/")
+public class SectionController {
+
+}
