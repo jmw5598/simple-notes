1bb4f47056db09d09a21aec768fee0ec6ea022c8
Jason White <jmw5598@gmail.com>
2018-04-14 10:46:49 -0400

    updated topic dashboard to show list of current topics

--- interface/src/app/topics/topic-dashboard/topic-dashboard.component.ts
+++ interface/src/app/topics/topic-dashboard/topic-dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TopicsService } from '../../core/services/topics.service';
+
+import { Topic } from '../../shared/model/topic.model';
+
 @Component({
   selector: 'sn-topic-dashboard',
   templateUrl: './topic-dashboard.component.html',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private topicsService: TopicsService) { }
+
+  topics: Topic[];
 
   ngOnInit() {
+    this.retrieveTopics();
+  }
+
+  onDelete(id: number) {
+    this.topicsService.delete(id)
+      .subscribe(
+        data => this.retrieveTopics(),
+        error => console.log("error deleting topic with id: " + id)
+      )
+  }
+
+  private retrieveTopics() {
+    this.topicsService.findAllTopics()
+      .subscribe(
+        data => {this.topics = data; console.log(this.topics)},
+        error => console.log(error)
+      );
   }
 
 }
