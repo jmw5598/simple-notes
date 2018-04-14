0520ac8747f51ae84f280c338ce3616e718655f6
Jason White <jmw5598@gmail.com>
2018-04-14 10:41:26 -0400

    added authentication guard to providers of authencation module

--- interface/src/app/authentication/authentication.module.ts
+++ interface/src/app/authentication/authentication.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 
+import { AuthenticationGuard } from './guards/authentication.guard';
 import { AuthenticationService } from './services/authentication.service';
 
 @NgModule({
   imports: [],
   exports: [],
-  declarations: [AuthenticationService],
-  providers: [AuthenticationService]
+  declarations: [],
+  providers: [
+    AuthenticationGuard,
+    AuthenticationService
+  ]
 })
 export class AuthenticationModule {}
