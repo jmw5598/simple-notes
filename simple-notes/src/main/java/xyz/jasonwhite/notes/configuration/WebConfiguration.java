2237bd1fa17a7472fff8dd89e2190aa64a1f3f53
Jason White <jmw5598@gmail.com>
2018-04-14 13:35:48 -0400

    created WebConfiguration class and added cors mappings to allow all HTTP Methods

---
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/configuration/WebConfiguration.java
@@ -0,0 +1,16 @@
+package xyz.jasonwhite.notes.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+
+}
