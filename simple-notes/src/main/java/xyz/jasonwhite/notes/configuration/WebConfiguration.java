5e87a7fbfc8710350d0f267d18faee224e8f32ec
Jason White <jmw5598@gmail.com>
2018-04-19 10:14:44 -0400

    added DocumentConverter bean

--- simple-notes/src/main/java/xyz/jasonwhite/notes/configuration/WebConfiguration.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/configuration/WebConfiguration.java
@@ -1,8 +1,11 @@
 package xyz.jasonwhite.notes.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import club.caliope.udc.DocumentConverter;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
@@ -11,6 +14,11 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+    
+    @Bean
+    public DocumentConverter documentConverter() {
+        return new DocumentConverter();
     }
 
 }
