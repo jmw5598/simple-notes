5e8f8a41bf7b46d1c6507c5f1a9276e8f79c94e6
Jason White <jmw5598@gmail.com>
2018-04-22 08:44:01 -0400

    added OutputFormatConverter to formatters

--- simple-notes/src/main/java/xyz/jasonwhite/notes/configuration/WebConfiguration.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/configuration/WebConfiguration.java
@@ -2,10 +2,12 @@ package xyz.jasonwhite.notes.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import club.caliope.udc.DocumentConverter;
+import xyz.jasonwhite.notes.converters.OutputFormatConverter;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
@@ -14,6 +16,11 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+    
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new OutputFormatConverter());;
     }
     
     @Bean
