dea6c46a141a29668f9117bd74e007bd7c478a7c
Jason White <jmw5598@gmail.com>
2018-04-22 08:44:45 -0400

    added handler to handle MethodArgumentTypeMismatchException

--- simple-notes/src/main/java/xyz/jasonwhite/notes/controllers/advice/TopicControllerAdvice.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/controllers/advice/TopicControllerAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import xyz.jasonwhite.notes.controllers.TopicController;
 import xyz.jasonwhite.notes.repositories.exceptions.TopicNotFoundException;
@@ -17,6 +18,11 @@ public class TopicControllerAdvice extends AbstractControllerAdvice {
     @ExceptionHandler(TopicNotFoundException.class)
     public ResponseEntity<VndErrors> handleBookNotFound(TopicNotFoundException e) {
         return error(e, HttpStatus.NOT_FOUND, String.valueOf(e.getId()));
+    }
+    
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<VndErrors> handleInvalidInputFormat(MethodArgumentTypeMismatchException e) {
+        return error(e, HttpStatus.NOT_ACCEPTABLE, e.getMessage());
     }
 
 }
