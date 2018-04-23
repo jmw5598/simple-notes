6f0a158704dafe96dcd0511f66507afc101d23b3
Jason White <jmw5598@gmail.com>
2018-04-23 19:21:53 -0400

    created override of findById factoring in authenticated user as owner

--- simple-notes/src/main/java/xyz/jasonwhite/notes/repositories/TopicRepository.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/repositories/TopicRepository.java
@@ -1,7 +1,10 @@
 package xyz.jasonwhite.notes.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import xyz.jasonwhite.notes.model.Topic;
@@ -12,5 +15,11 @@ public interface TopicRepository extends CrudRepository<Topic, Long> {
     @Override
     @Query("SELECT t FROM Topic t WHERE t.owner = ?#{ authentication.name }")
     Iterable<Topic> findAll();
+
+    @Override
+    @Query("SELECT t FROM Topic t WHERE t.id = :id AND t.owner = ?#{ authentication.name }")
+    Optional<Topic> findById(@Param("id") Long id);
+    
+    
  
 }
