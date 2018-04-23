1619b29fb185bb37ccd00e837e98aac93683f174
Jason White <jmw5598@gmail.com>
2018-04-23 16:14:14 -0400

    added post filter to findall

--- simple-notes/src/main/java/xyz/jasonwhite/notes/repositories/TopicRepository.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/repositories/TopicRepository.java
@@ -1,6 +1,7 @@
 package xyz.jasonwhite.notes.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.stereotype.Repository;
 
 import xyz.jasonwhite.notes.model.Topic;
@@ -8,4 +9,8 @@ import xyz.jasonwhite.notes.model.Topic;
 @Repository
 public interface TopicRepository extends CrudRepository<Topic, Long> {
 
+    @Override
+    @PostFilter(value = "filterObject.owner == authentication.name") // inefficient on large lists
+    Iterable<Topic> findAll();
+ 
 }
