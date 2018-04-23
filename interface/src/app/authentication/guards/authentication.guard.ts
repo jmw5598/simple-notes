3d77757892d9db6e80597845bd53593ba6b4ac8e
Jason White <jmw5598@gmail.com>
2018-04-23 14:05:00 -0400

    updated authentication guard

--- interface/src/app/authentication/guards/authentication.guard.ts
+++ interface/src/app/authentication/guards/authentication.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
@@ -6,37 +6,24 @@ import { Subscription } from 'rxjs/Subscription';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
-export class AuthenticationGuard implements CanActivate, OnDestroy {
+export class AuthenticationGuard implements CanActivate {
 
-  private subscription: Subscription;
   isAuthenticated: boolean;
-
 
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService
-  ) {
-    this.authenticationService.authentication
-      .subscribe(
-        data => this.isAuthenticated = data,
-        error => console.log("error subscibing to authencatio form auth guard")
-      )
-  }
+  ) {}
 
   canActivate(
       next: ActivatedRouteSnapshot,
       state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if(this.isAuthenticated) {
-      return this.isAuthenticated;
+    if(this.authenticationService.isAuthenticated()) {
+      return true;
     } else {
       this.router.navigate(['login']);
       return false;
     }
-  }
-
-  ngOnDestroy() {
-    if(this.subscription)
-      this.subscription.unsubscribe();
   }
 
 }
