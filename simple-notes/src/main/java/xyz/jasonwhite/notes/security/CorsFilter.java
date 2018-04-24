4691d537082bb4388503b9e2681c0576e2d80dc9
Jason White <jmw5598@gmail.com>
2018-04-24 11:51:37 -0400

    created cors filter

---
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/security/CorsFilter.java
@@ -0,0 +1,36 @@
+package xyz.jasonwhite.notes.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CorsFilter extends OncePerRequestFilter {
+    
+    static final String ORIGIN = "Origin";
+    
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, 
+            HttpServletResponse response, 
+            FilterChain filterChain) throws ServletException, IOException {
+        
+        String origin = request.getHeader(ORIGIN);
+        
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");//* or origin as u prefer
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE, PATCH");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "content-type, authorization");
+        
+        if (request.getMethod().equals("OPTIONS"))
+            response.setStatus(HttpServletResponse.SC_OK);
+        else 
+            filterChain.doFilter(request, response);
+        
+    }
+}
