c9f283cb7aa2db0c9fcaf8421e997817f28f392e
Jason White <jmw5598@gmail.com>
2018-04-24 10:14:49 -0400

    updated topic controller

--- simple-notes/src/main/java/xyz/jasonwhite/notes/controllers/TopicController.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/controllers/TopicController.java
@@ -1,40 +1,46 @@
 package xyz.jasonwhite.notes.controllers;
 
-import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import xyz.jasonwhite.notes.controllers.resources.SectionResource;
+import club.caliope.udc.InputFormat;
+import club.caliope.udc.OutputFormat;
 import xyz.jasonwhite.notes.controllers.resources.TopicResource;
-import xyz.jasonwhite.notes.model.Section;
 import xyz.jasonwhite.notes.model.Topic;
-import xyz.jasonwhite.notes.repositories.SectionRepository;
 import xyz.jasonwhite.notes.repositories.TopicRepository;
-import xyz.jasonwhite.notes.repositories.exceptions.SectionNotFoundException;
 import xyz.jasonwhite.notes.repositories.exceptions.TopicNotFoundException;
+import xyz.jasonwhite.notes.services.DocumentService;
+import xyz.jasonwhite.notes.utilities.TopicExportUtility;
 
 @RestController
 @RequestMapping("/v1/topics")
 public class TopicController {
     
     private TopicRepository topicRepository;
-    private SectionRepository sectionRepository;
+    private DocumentService documentService;
     
     @Autowired
-    public TopicController(TopicRepository topicRepository, SectionRepository sectionRepository) {
+    public TopicController(TopicRepository topicRepository, DocumentService documentService) {
         this.topicRepository = topicRepository;
-        this.sectionRepository = sectionRepository;
+        this.documentService = documentService;
     }
     
     @GetMapping
@@ -43,7 +49,9 @@ public class TopicController {
     }
     
     @PostMapping
-    public ResponseEntity<TopicResource> createTopic(@RequestBody Topic topic) {
+    public ResponseEntity<TopicResource> createTopic(@RequestBody Topic topic, Authentication authentication) {
+        String user = authentication.getName();
+        topic.setOwner(user);
         Topic newTopic = this.topicRepository.save(topic);
         return ResponseEntity.ok(new TopicResource(newTopic));
     }
@@ -56,7 +64,7 @@ public class TopicController {
     }
     
     @DeleteMapping(path="/{topicId}")
-    public ResponseEntity<?> deleteBook(@PathVariable("topicId") Long topicId) {
+    public ResponseEntity<?> deleteTopic(@PathVariable("topicId") Long topicId) {
         return this.topicRepository.findById(topicId)
             .map( p -> {
                 this.topicRepository.deleteById(topicId);
@@ -65,47 +73,13 @@ public class TopicController {
             .orElseThrow(() -> new TopicNotFoundException(topicId));
     }
     
-    @GetMapping(path="/{topicId}/sections")
-    public ResponseEntity<Set<Section>> getChapters(@PathVariable("topicId") Long topicId) {
-        return this.topicRepository.findById(topicId)
-            .map(b -> ResponseEntity.ok(b.getSections()))
-            .orElseThrow(() -> new TopicNotFoundException(topicId));
-    }
-    
-    @PostMapping(path="/{topicId}/sections")
-    public  ResponseEntity<?> createSection(
-            @PathVariable("topicId") Long topicId, @RequestBody Section section) {
+    @GetMapping(path="/{topicId}/export", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<?> exportTopic(
+            @PathVariable("topicId") Long topicId,
+            @RequestParam(value="fileType", defaultValue="txt") OutputFormat fileType) throws IOException {
         
-        return this.topicRepository.findById(topicId)
-            .map(b -> {
-                b.addSection(section);
-                this.topicRepository.save(b);
-                return ResponseEntity.noContent().build();
-            })
-            .orElseThrow(() -> new TopicNotFoundException(topicId));
+        return null;
         
-    }
-    
-    @GetMapping(path="/{topicId}/sections/{sectionId}")
-    public ResponseEntity<Section> getSection(
-            @PathVariable("topicId") Long topicId, @PathVariable("sectionId") Long sectionId) throws Exception {
-        
-        return this.sectionRepository.findById(sectionId)
-            .map(s -> ResponseEntity.ok(s))
-            .orElseThrow(() -> new SectionNotFoundException(sectionId));
-    }
-    
-    @PutMapping(path="/{topicId}/sections/{sectionId}")
-    public ResponseEntity<Section> updateSection(
-            @PathVariable("topicId") Long topicId, @PathVariable("sectionId") Long sectionId, @RequestBody Section section) {
-
-        return this.sectionRepository.findById(sectionId)
-            .map(s -> {
-                BeanUtils.copyProperties(section, s);
-                this.sectionRepository.save(s);
-                return ResponseEntity.ok(s);
-            })
-            .orElseThrow(() -> new SectionNotFoundException(sectionId));
     }
     
 }
