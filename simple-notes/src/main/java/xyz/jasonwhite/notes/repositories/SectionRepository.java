dd8f694bf834179624fdd6fa8165b1418d995662
Jason White <jmw5598@gmail.com>
2018-04-24 10:59:37 -0400

    added order by to query (by title and created on in asc)

--- simple-notes/src/main/java/xyz/jasonwhite/notes/repositories/SectionRepository.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/repositories/SectionRepository.java
@@ -13,7 +13,7 @@ import xyz.jasonwhite.notes.model.Topic;
 @Repository
 public interface SectionRepository extends CrudRepository<Section, Long> {
     
-    @Query("SELECT s FROM Section s WHERE s.topic = :topic AND s.topic.owner = ?#{ authentication.name }")
+    @Query("SELECT s FROM Section s WHERE s.topic = :topic AND s.topic.owner = ?#{ authentication.name } ORDER BY s.title ASC, s.createdOn ASC")
     Iterable<Section> findAllByTopic(@Param("topic") Topic topic);
     
     @Query("SELECT s FROM Section s WHERE s.id = :id AND s.topic = :topic AND s.topic.owner = ?#{ authentication.name }")
