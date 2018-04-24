23cb596313eac23c1b26b538aa9c5a21278692c0
Jason White <jmw5598@gmail.com>
2018-04-24 12:36:49 -0400

    created deleteAllByTopic method to remove all section by topic

--- simple-notes/src/main/java/xyz/jasonwhite/notes/repositories/SectionRepository.java
+++ simple-notes/src/main/java/xyz/jasonwhite/notes/repositories/SectionRepository.java
@@ -2,9 +2,13 @@ package xyz.jasonwhite.notes.repositories;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import xyz.jasonwhite.notes.model.Section;
@@ -18,5 +22,11 @@ public interface SectionRepository extends CrudRepository<Section, Long> {
     
     @Query("SELECT s FROM Section s WHERE s.id = :id AND s.topic = :topic AND s.topic.owner = ?#{ authentication.name }")
     Optional<Section> findByIdAndTopic(@Param("id") Long id, @Param("topic") Topic topic);
+    
+    @Modifying
+    @Transactional
+    @PreAuthorize("#topic.owner == authentication.name")
+    @Query("DELETE FROM Section s WHERE s.topic = :topic")
+    void deleteAllByTopic(@Param("topic") Topic topic);
 
 }
