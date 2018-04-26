471f15cb2f134f8bea9517ff970bb2bba0380cbc
Jason White <jmw5598@gmail.com>
2018-04-26 10:05:39 -0400

    added spinner declaration and export, imported common module

--- interface/src/app/shared/shared.module.ts
+++ interface/src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
-  imports: [],
-  exports: [],
-  declarations: [],
+  imports: [CommonModule],
+  exports: [SpinnerComponent],
+  declarations: [SpinnerComponent],
   providers: []
 })
 export class SharedModule {}
