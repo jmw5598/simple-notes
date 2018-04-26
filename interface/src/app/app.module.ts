9ef3b081dadd382d68e136d31e2dbf4307f94013
Jason White <jmw5598@gmail.com>
2018-04-26 10:01:08 -0400

    added modules for forms, reactive forms, and shared

--- interface/src/app/app.module.ts
+++ interface/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app.routing';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { CoreModule } from './core/core.module';
 import { NavigationModule } from './navigation/navigation.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -20,7 +22,10 @@ import { LoginComponent } from './login/login.component';
     AuthenticationModule,
     BrowserModule,
     CoreModule,
+    FormsModule,
     NavigationModule,
+    ReactiveFormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
