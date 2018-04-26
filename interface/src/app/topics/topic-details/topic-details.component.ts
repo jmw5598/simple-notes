e5df5ff68c2e48e84b956a658a3bff5e2814d2e0
Jason White <jmw5598@gmail.com>
2018-04-26 10:02:59 -0400

    updated topic details component

--- interface/src/app/topics/topic-details/topic-details.component.ts
+++ interface/src/app/topics/topic-details/topic-details.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
+import { saveAs } from 'file-saver/FileSaver';
+
+import { FileType } from '../../shared/model/file-type.enum';
+import { ModalOptions } from '../topic-export/modal-options.model';
+import { SectionsService } from '../../core/services/sections.service';
 import { Topic } from '../../shared/model/topic.model';
 import { TopicsService } from '../../core/services/topics.service';
 
@@ -15,36 +20,73 @@ export class TopicDetailsComponent implements OnInit, OnDestroy {
   private params: Subscription;
   private id: number;
   public topic: Topic;
+  public exportModalOptions: ModalOptions;
 
   constructor(
     private route: ActivatedRoute,
+    private sectionsService: SectionsService,
     private topicsService: TopicsService
-  ) { }
+  ) {
+    this.exportModalOptions = new ModalOptions(false, false);
+  }
 
   ngOnInit() {
     this.params = this.route.params
       .subscribe(
         params => {
           this.id = +params['id']; // (+) converts string 'id' to a number
-          this.retrieveData();
+          this.retrieveTopic();
         }
       );
   }
 
   delete(sectionId: number) {
-    this.topicsService.deleteSection(this.topic.id, sectionId)
+    this.sectionsService.delete(this.topic.id, sectionId)
       .subscribe(
-        data => this.retrieveData(),
+        data => this.retrieveSections(),
         error => console.log("error deleting section with id: " + sectionId)
       );
   }
 
-  retrieveData() {
+  export(event: any) {
+    if(event.confirm) {
+      this.exportModalOptions.isBusy = true;
+      this.topicsService.export(this.id, event.options)
+        .subscribe(
+          data => {
+            this.exportModalOptions.isBusy = false;
+            saveAs(data.blob, data.filename);
+          },
+          error => {
+            console.log(error);
+            this.exportModalOptions.isBusy = false;
+          }
+        );
+
+    } else {
+      this.exportModalOptions.isShown = false;
+    }
+  }
+
+  showExportModal() {
+    this.exportModalOptions.isShown = true;
+  }
+
+  retrieveTopic() {
     this.topicsService.findById(this.id)
       .subscribe(
-        data => this.topic = data,
-        error => console.log("error getting data for id: " + this.id)
+        data => {
+          this.topic = data.topic;
+          this.retrieveSections();
+        }
       );
+  }
+
+  retrieveSections() {
+    this.sectionsService.findAll(this.id)
+      .subscribe(
+        data => this.topic.sections = data
+      )
   }
 
   ngOnDestroy() {
