f7cda6d51988ca6f465c6f3a59204482c6abcd2b
Jason White <jmw5598@gmail.com>
2018-04-26 10:08:48 -0400

    added JwtModule, HttpClientModule imports; added providers for token interceptor, JwtModule

--- interface/src/app/authentication/authentication.module.ts
+++ interface/src/app/authentication/authentication.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AuthenticationGuard } from './guards/authentication.guard';
 import { AuthenticationService } from './services/authentication.service';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
-  imports: [],
+  imports: [JwtModule, HttpClientModule],
   exports: [],
   declarations: [],
   providers: [
     AuthenticationGuard,
-    AuthenticationService
+    AuthenticationService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    },
+    JwtModule
   ]
 })
 export class AuthenticationModule {}
