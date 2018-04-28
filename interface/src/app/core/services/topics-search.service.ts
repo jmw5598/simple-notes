4914169eb03fcb375a0f67befd2701cebf4788a0
Jason White <jmw5598@gmail.com>
2018-04-28 12:07:32 -0400

    created topic search service, used observable to share results between navbar search field (eventually) and topic-search component

--- interface/src/app/core/services/topics-search.service.ts
+++ interface/src/app/core/services/topics-search.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { Category } from '../../shared/model/category.model';
+import { Topic } from '../../shared/model/topic.model';
+import { TopicsService } from './topics.service';
 
 @Injectable()
 export class TopicsSearchService {
 
-  constructor() { }
+  private categoriesSubject = new BehaviorSubject<Category[]>(null);
+  private resultsSubject = new BehaviorSubject<Topic[]>(null);
+
+  public categories = this.categoriesSubject.asObservable();
+  public results = this.resultsSubject.asObservable();
+
+  constructor(private topicsService: TopicsService) { }
+
+  search(categories: Category[]) {
+    this.topicsService.search(categories)
+      .subscribe(
+        data => {
+          this.categoriesSubject.next(categories);
+          this.resultsSubject.next(data);
+        },
+        error => console.log("error searching for categories inside topics search service")
+      )
+  }
 
 }
