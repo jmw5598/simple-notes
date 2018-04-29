07f20f3fe0b8d7335173333fe4fb416aed014343
Jason White <jmw5598@gmail.com>
2018-04-29 08:19:25 -0400

    updated topic-search component

--- interface/src/app/topics/topic-search/topic-search.component.ts
+++ interface/src/app/topics/topic-search/topic-search.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Category } from '../../shared/model/category.model';
+import { Topic } from '../../shared/model/topic.model';
+import { TopicsSearchService } from './topics-search.service';
 
 @Component({
-  selector: 'app-topic-search',
+  selector: 'sn-topic-search',
   templateUrl: './topic-search.component.html',
   styleUrls: ['./topic-search.component.css']
 })
-export class TopicSearchComponent implements OnInit {
+export class TopicSearchComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private categoriesSubscription: Subscription;
+  private resultsSubscription: Subscription;
+
+  categories: Category[];
+  results: Topic[];
+  tags: string[] = [];
+
+  constructor(private topicsSearchService: TopicsSearchService) {
+
+  }
 
   ngOnInit() {
+
+    this.categoriesSubscription = this.topicsSearchService.categories
+      .subscribe(
+        data => {
+          this.categories = data;
+          let tags: string[] = [];
+          this.categories.forEach(e => tags.push(e.description));
+          this.tags = tags;
+        },
+        error => console.log("error subscribing to topic search categories")
+      );
+
+    this.resultsSubscription = this.topicsSearchService.results
+      .subscribe(
+        data => this.results = data,
+        error => console.log("error subscribing to topic search results")
+      );
+
+  }
+
+  search() {
+    this.topicsSearchService.search(this.categories)
+  }
+
+  onRemove(tag: string) {
+    this.topicsSearchService.removeCategory(tag);
+    this.topicsSearchService.search(this.categories);
+    console.log(tag);
+  }
+
+  onAdd(tag: string) {
+    this.topicsSearchService.addCategory(tag);
+    this.topicsSearchService.search(this.categories);
+    console.log(tag);
+  }
+
+  ngOnDestroy() {
+    if(this.categoriesSubscription)
+      this.categoriesSubscription.unsubscribe();
+    if(this.resultsSubscription)
+      this.resultsSubscription.unsubscribe();
   }
 
 }
