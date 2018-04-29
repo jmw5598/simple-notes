139f33c3615357a35cfb168da0bd904aeb49196f
Jason White <jmw5598@gmail.com>
2018-04-29 08:34:12 -0400

    created wrapper navigation component to hold both top and side navbars. exported wrapper navigation in navigation module

--- interface/src/app/navigation/navigation.module.ts
+++ interface/src/app/navigation/navigation.module.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarSideComponent } from './navbar-side/navbar-side.component';
+import { NavigationComponent } from './navigation.component';
 
 @NgModule({
   imports: [RouterModule],
-  exports: [NavbarComponent, NavbarSideComponent],
-  declarations: [NavbarComponent, NavbarSideComponent],
+  exports: [NavigationComponent],
+  declarations: [NavbarComponent, NavbarSideComponent, NavigationComponent],
   providers: []
 })
 export class NavigationModule {}
