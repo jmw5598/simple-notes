c2bd4f304bf19054b545aad8666ab09aed4a0af9
Jason White <jmw5598@gmail.com>
2018-04-29 09:51:32 -0400

    fixed selector closing tag error, updated navigation module with common import and navbar side service as provider

--- interface/src/app/navigation/navigation.module.ts
+++ interface/src/app/navigation/navigation.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarSideComponent } from './navbar-side/navbar-side.component';
+import { NavbarSideService } from './navbar-side/navbar-side.service';
 import { NavigationComponent } from './navigation.component';
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   exports: [NavigationComponent],
   declarations: [NavbarComponent, NavbarSideComponent, NavigationComponent],
-  providers: []
+  providers: [NavbarSideService]
 })
 export class NavigationModule {}
