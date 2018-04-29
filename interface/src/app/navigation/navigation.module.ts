eef47f5a74532603bf8c0f5ebc2cd22a108ad88a
Jason White <jmw5598@gmail.com>
2018-04-29 08:27:50 -0400

    initial commit of side nave bar component, updated navigation module

--- interface/src/app/navigation/navigation.module.ts
+++ interface/src/app/navigation/navigation.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarSideComponent } from './navbar-side/navbar-side.component';
 
 @NgModule({
   imports: [RouterModule],
-  exports: [NavbarComponent],
-  declarations: [NavbarComponent],
+  exports: [NavbarComponent, NavbarSideComponent],
+  declarations: [NavbarComponent, NavbarSideComponent],
   providers: []
 })
 export class NavigationModule {}
